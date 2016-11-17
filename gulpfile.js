27a230d7f53c748d0e5ca194f8f06d13a498a7e7
furkick92 <furkick92@gmail.com>
2016-11-17 22:52:55 +0000

    Added minified resources, created gulpfile and created dist folder

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-mini-css');
+
+gulp.task('minifyJS', function () {
+    gulp.src(['resources/js/ngTile.js'])
+        .pipe(ngAnnotate())
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(uglify())
+        .pipe(rename('ngTile.min.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minifyCSS', function () {
+    gulp.src(['resources/css/ngTile.css'])
+        .pipe(minifyCSS())
+        .pipe(rename('ngTile.min.css'))
+        .pipe(gulp.dest('dist'))
+});
